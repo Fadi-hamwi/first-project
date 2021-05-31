27302cb11c54fd1b975b2ecfab7d8adb63cbdadb
Fadi-hamwi <69624793+Fadi-hamwi@users.noreply.github.com>
2021-05-31 20:14:32 +0300

    Add files via upload

---
+++ Largest Palindrome Product.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int ans = 0;
+	for(int i=999; i>=101;--i){
+		for(int j=i;j >= 101;--j){
+			int product = j*i;
+			//cout << i << " " << j << endl;
+			string original = to_string(product);
+			string reversed = original;
+			reverse(reversed.begin(), reversed.end());
+			
+			if(reversed == original){
+				ans = max(ans, i*j);
+			}
+		}
+		
+	}
+	cout << ans << endl;
+	return 0;
+	
+}
