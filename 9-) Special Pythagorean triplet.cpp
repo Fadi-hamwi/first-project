ce02a26544903efbfb995e560b54487c71fb936b
Fadi-hamwi <69624793+Fadi-hamwi@users.noreply.github.com>
2021-06-07 20:16:13 +0300

    Update and rename Special Pythagorean triplet.cpp to 9-) Special Pythagorean triplet.cpp

--- 9-) Special Pythagorean triplet.cpp
+++ 9-) Special Pythagorean triplet.cpp
@@ -3,7 +3,8 @@
 using namespace std;
 
 int main(){
-	for(int i =1; i < 333;++i){
+	for(int i =1; i < 333;++i){          /* If we divided 1000 by 3 we will get 333 and because a < b < c and not equal ,
+						so the first number have to be less than 333,the second one is less than 666, and the third one is less than 1000 */
 		for(int j = i+1; j < 666;++j){
 			for(int z = j+1; z < 1000; ++z){
 				if(i + j + z == 1000 && pow(z, 2) == pow(i, 2) + pow(j, 2)){
